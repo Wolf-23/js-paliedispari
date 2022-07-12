25a4806f73200a9b85ee541a996654851b24b93b
Riccardo <lupinu.ric@gmail.com>
2022-07-12 18:42:29 +0200

    Project: END

--- PariDispari/js/main.js
+++ PariDispari/js/main.js
@@ -5,24 +5,19 @@ let numero = parseInt(prompt('Inserisci un numero da 1 a 5:'));
 let min = 0;
 let max = 5;
 
-let num = 0;
-
 
 let numeroPc = numRandom(min, max);
 let sommaNumeri = numero + numeroPc;
+alert(`La somma è ${sommaNumeri}`);
 console.log(numero, numeroPc, sommaNumeri, numRandom(min, max));
-
-
 
 let pariDispari = controllo(sommaNumeri);
 
-if (scelta == pariDispari) {
+if (scelta.toUpperCase() == pariDispari.toUpperCase()) {
     alert('Hai Vinto!')
 } else {
     alert('Ha vinto il computer!')
 }
-
-
 
 function numRandom (min, max) {
     let RandomNumber = Math.floor(Math.random() * max) + min;
